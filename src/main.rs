a6a9c74788ec445ab776c12fe18febcb349b3911
tenax66 <hiyoshir+tenax66@gmail.com>
2023-11-14 23:44:01 +0900

    make this program a CLI tool

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+use clap::Parser;
 use qrcode_generator::QrCodeEcc;
 use web_view::*;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(help = "The word you want to encode")]
+    word: String,
+}
+
 fn main() {
+    let args = Args::parse();
     // word to encode
-    let word = "example.com";
 
     let result: String =
-        qrcode_generator::to_svg_to_string(word, QrCodeEcc::Low, 256, None::<&str>).unwrap();
+        qrcode_generator::to_svg_to_string(args.word, QrCodeEcc::Low, 256, None::<&str>).unwrap();
 
     // centering svg image
     let html = format!(
