9ca98a0874626f9030951f0827b4843656c0097e
tenax66 <hiyoshir+tenax66@gmail.com>
2023-11-14 23:23:03 +0900

    centering image

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,24 @@ fn main() {
     let result: String =
         qrcode_generator::to_svg_to_string(word, QrCodeEcc::Low, 256, None::<&str>).unwrap();
 
+    // centering svg image
+    let html = format!(
+        r#"
+            <style>
+                svg {{
+                    display: block;
+                    margin: auto;
+                }}
+            </style>
+            {}
+            "#,
+        result
+    );
+
     web_view::builder()
         .title("QR Code Viewer")
-        .content(Content::Html(result))
-        .size(256, 256)
+        .content(Content::Html(html))
+        .size(300, 280)
         .resizable(true)
         .debug(true)
         .user_data(())
